0ca32b4e35ccbf9a2673aec2305db8dc3a911a1b
Yu Qi Liu <yu.qi.liu236@gmail.com>
2019-06-12 14:58:21 -0400

    redux init store, reducer, action, actionCreator #3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
+import Store from '../store';
 
 const SearchBar = props => {
   return (
-    <div className="search-bar-area">
-      <input type="text" placeholder="Type to search..." />
-    </div>
+    <Provider store={Store}>
+      <div className="search-bar-area">
+        <input type="text" placeholder="Type to search..." />
+      </div>
+    </Provider>
   );
-}
+};
 
 export default SearchBar;
