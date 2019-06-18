8ab823102ba5c651631d7922fc53e7712ba820f2
Vincent Boivin <vboivin04@gmail.com>
2019-06-18 19:30:37 -0400

    Trying to implement best practices #GSM-12
    Using a container component to take care of the
    side effects.

--- src/app/components/MainBodyTopArea/MainBodyTopArea.jsx
+++ src/app/components/MainBodyTopArea/MainBodyTopArea.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Graph from './Graph';
 import LatestNews from './LatestNews';
+import {
+  getError,
+  getLoading,
+  getTimeSeries,
+  getTopNews,
+} from '../../../reducers/reducer';
+import AdaptiveLoader from '../loader/adaptiveLoader';
 
-const MainBodyTopArea = () => {
+const MainBodyTopArea = ({ news, data, loading, error }) => {
   return (
     <div className="main-body-top-area">
-      <Graph />
-      <LatestNews />
+      {loading && <AdaptiveLoader />}
+      {error && <div>Error : {error}</div>}
+      {data && !loading && <Graph data={prepData(data.fiveD)} />}
+      {news && !loading && <LatestNews news={prepNews(news)} />}
     </div>
   );
 };
 
-export default MainBodyTopArea;
+const prepNews = news => {
+  return news.map(n => {
+    return (
+      <div className="news" key={`news-${n.headline}`}>
+        <span>{n.headline}</span>
+        <span>{n.summary}</span>
+      </div>
+    );
+  });
+};
+
+const prepData = data => {
+  return data.map(d => {
+    return { x: new Date(d.date).toDateString(), y: d.close };
+  });
+};
+
+const mapStateToProps = state => ({
+  data: getTimeSeries(state),
+  news: getTopNews(state),
+  error: getError(state),
+  loading: getLoading(state),
+});
+
+export default connect(mapStateToProps)(MainBodyTopArea);
